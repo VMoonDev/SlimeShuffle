5247fa6392544b749fb2612d0c76341c552df82c
Victoria Granger <vanastasiamoon@gmail.com>
2022-10-01 15:56:12 -0400

    Refactor Pawn.gd

--- project/Pawn/Pawn.gd
+++ project/Pawn/Pawn.gd
@@ -9,15 +9,9 @@ var _velocity := Vector2.ZERO
 var _wall_jumped := false
 
 func _physics_process(delta):
-	##_velocity.y = 0 if is_on_floor() else _velocity.y + _GRAVITY * delta
-	if is_on_floor():
-		_velocity.y = 0
-	elif is_on_wall() and _velocity.y > 0:
-		_velocity.y += _SLIDESPEED * delta
-	else:
-		_velocity.y += _GRAVITY * delta
+	_gravity(delta)
 	_movement()
-	_velocity = move_and_slide(_velocity, Vector2.UP)
+
 
 func _movement():
 	if is_on_floor():
@@ -28,3 +22,13 @@ func _movement():
 	if Input.is_action_just_pressed("jump") and is_on_wall() and _wall_jumped == false:
 		_wall_jumped = true
 		_velocity.y = -_JUMPSTRENGTH
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+
+		
+func _gravity(delta):
+	if is_on_floor():
+		_velocity.y = 0
+	elif is_on_wall() and _velocity.y > 0 or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
+		_velocity.y += _SLIDESPEED * delta
+	else:
+		_velocity.y += _GRAVITY * delta
