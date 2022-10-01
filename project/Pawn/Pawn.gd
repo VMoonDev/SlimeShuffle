c161b80d4344192a9886649f69111878c1c144c0
Victoria Granger <vanastasiamoon@gmail.com>
2022-10-01 12:49:33 -0400

    Add movement to pawn

--- project/Pawn/Pawn.gd
+++ project/Pawn/Pawn.gd
@@ -1,10 +1,17 @@
 extends KinematicBody2D
 
 const _GRAVITY := 200
+const _MOVESPEED := 200
+const _JUMPSTRENGTH := 200
 
 var _velocity := Vector2.ZERO
 
 func _physics_process(delta):
-	_velocity.y += _GRAVITY * delta
-	
-	move_and_slide(_velocity)
+	_velocity.y = 0 if is_on_floor() else _velocity.y + _GRAVITY * delta
+	_movement()
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+
+func _movement():
+	_velocity.x = Input.get_axis("move_left", "move_right") * _MOVESPEED
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		_velocity.y = -_JUMPSTRENGTH
