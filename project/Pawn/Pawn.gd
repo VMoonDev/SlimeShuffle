93945c26cb0777000b3a1ea154b5287b2b1d63de
Victoria Granger <vanastasiamoon@gmail.com>
2022-10-01 21:10:22 -0400

    Clean up Pawn.gd

--- project/Pawn/Pawn.gd
+++ project/Pawn/Pawn.gd
@@ -12,6 +12,7 @@ var _is_attacking := false
 var _wall_jumped := 0
 var wall_jump_max := 3 setget wall_jump_max_set
 
+
 func _physics_process(delta):
 	_gravity(delta)
 	if _is_attacking == false:
@@ -20,21 +21,22 @@ func _physics_process(delta):
 			_jump()
 
 
-func _movement():
-	_velocity.x = Input.get_axis("move_left", "move_right") * _MOVESPEED
-	_velocity = move_and_slide(_velocity, Vector2.UP)
-
-		
 func _gravity(delta):
 	if is_on_floor():
 		_velocity.y = 0
 		_wall_jumped = 0
 		_is_jumping = false
-	elif is_on_wall() and _velocity.y > 0 or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
+	elif is_on_wall() and _velocity.y > 0 and Input.get_axis("move_left", "move_right") != 0:
 		_velocity.y += _SLIDESPEED * delta
 	else:
 		_velocity.y += _GRAVITY * delta
-		
+
+
+func _movement():
+	_velocity.x = Input.get_axis("move_left", "move_right") * _MOVESPEED
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+
+
 func _jump():
 	if _is_jumping == false:
 		_is_jumping = true
@@ -45,16 +47,15 @@ func _jump():
 	if is_on_wall() and _wall_jumped == wall_jump_max:
 		_wall_jumped += 1
 		_velocity.y = -_SCRAMBLESTRENGTH
-	_velocity = move_and_slide(_velocity, Vector2.UP)	
+	_velocity = move_and_slide(_velocity, Vector2.UP)
 
-		
+
+func wall_jump_max_set(new_value):
+	wall_jump_max = new_value
+
+
 func _attack():
 	if Input.is_action_just_pressed("attack"):
 		_velocity.x *= 2
 		
-func wall_jump_max_set(new_value):
-	wall_jump_max = new_value
-	
-		
-func _power_up():
-	wall_jump_max = 20
+
