933a6965b60ceecb6c44bff3c4f82466fd3d7a2d
Victoria Granger <vanastasiamoon@gmail.com>
2022-10-01 20:41:42 -0400

    Refactor movement code on pawn

--- project/Pawn/Pawn.gd
+++ project/Pawn/Pawn.gd
@@ -3,40 +3,46 @@ extends KinematicBody2D
 const _GRAVITY := 800
 const _MOVESPEED := 400
 const _JUMPSTRENGTH := 450
+const _SCRAMBLESTRENGTH := 100
 const _SLIDESPEED := 50
 
 var _velocity := Vector2.ZERO
 var _is_jumping := false
+var _is_attacking := false
 var _wall_jumped := 0
 var wall_jump_max := 2 setget wall_jump_max_set
 
 func _physics_process(delta):
 	_gravity(delta)
-	_attack()
-	_movement()
+	if _is_attacking == false:
+		_movement()
+		if Input.is_action_just_pressed("jump"):
+			_jump()
 
 
 func _movement():
 	_velocity.x = Input.get_axis("move_left", "move_right") * _MOVESPEED
-	if is_on_floor():
-		_is_jumping = false
-		_wall_jumped = 0
-	if Input.is_action_just_pressed("jump") and _is_jumping == false:
-		_is_jumping = true
-		_velocity.y = -_JUMPSTRENGTH
-	if Input.is_action_just_pressed("jump") and is_on_wall() and _wall_jumped < wall_jump_max:
-		_wall_jumped += 1
-		_velocity.y = -_JUMPSTRENGTH
 	_velocity = move_and_slide(_velocity, Vector2.UP)
 
 		
 func _gravity(delta):
 	if is_on_floor():
 		_velocity.y = 0
+		_wall_jumped = 0
+		_is_jumping = false
 	elif is_on_wall() and _velocity.y > 0 or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right"):
 		_velocity.y += _SLIDESPEED * delta
 	else:
 		_velocity.y += _GRAVITY * delta
+		
+func _jump():
+	if _is_jumping == false:
+		_is_jumping = true
+		_velocity.y = -_JUMPSTRENGTH
+	if is_on_wall() and _wall_jumped < wall_jump_max:
+		_wall_jumped += 1
+		_velocity.y = -_JUMPSTRENGTH	
+	_velocity = move_and_slide(_velocity, Vector2.UP)	
 
 		
 func _attack():
